17397e613678952d1074d573367d1f7daf8a8320
msznober <61661382+msznober@users.noreply.github.com>
2021-02-18 21:51:44 +0100

    logika aplikacji

--- WpfAppProj/UpdatePage.xaml.cs
+++ WpfAppProj/UpdatePage.xaml.cs
@@ -19,9 +19,25 @@ namespace WpfAppProj
     /// </summary>
     public partial class UpdatePage : Window
     {
-        public UpdatePage()
+        wpfAppEntities _db = new wpfAppEntities();
+        int Id;
+        public UpdatePage(int memberId)
         {
             InitializeComponent();
+            Id = memberId;
+        }
+
+        private void updateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            member updateMember = (from m in _db.members
+                                  where m.id == Id
+                                  select m).Single();
+            updateMember.name = nameTextBox.Text;
+            updateMember.gender = gendercomboBox.Text;
+
+            _db.SaveChanges();
+            MainWindow.datagrid.ItemsSource = _db.members.ToList();
+            this.Hide();
         }
     }
 }
