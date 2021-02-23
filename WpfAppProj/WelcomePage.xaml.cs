49cdfd8ca6be1fa1c8f69fe0dfed2e73ebf5b832
msznober <61661382+msznober@users.noreply.github.com>
2021-02-23 19:19:01 +0100

    utworzenie okna powitalnego

---
+++ WpfAppProj/WelcomePage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfAppProj
+{
+    /// <summary>
+    /// Logika interakcji dla klasy WelcomePage.xaml
+    /// </summary>
+    public partial class WelcomePage : Window
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void goBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            MainWindow Ipage = new MainWindow();
+            Ipage.ShowDialog();
+        }
+    }
+}
